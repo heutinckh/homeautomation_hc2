a7cc72f80ccf7529202c475d9cda56f3e28ce1bc
H.Heutinck <heutinck.h@gmail.com>
2019-02-06 20:04:31 +0100

    Preparation Script added

---
+++ prepare.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Prepare the build and deploy environment
+
+set -e 
+
+# Parameters
+CWD=`pwd`
+YOCTO_FLAVOUR="thud"
+YOCTO_DIR="yocto-odroid"
+
+# Checks
+if [ -d ${YOCTO_DIR} ]; then
+    echo "Error: Existing directory: ${YOCTO_DIR} found, please remove first..."
+    exit 1
+fi
+
+# Do Work
+git clone -b ${YOCTO_FLAVOUR} git://git.yoctoproject.org/poky.git ${YOCTO_DIR}
+cd ${YOCTO_DIR}
+git clone -b ${YOCTO_FLAVOUR} git://github.com/akuster/meta-odroid
+
+# Move to version supporting odroid c2 (workaround for nice branch/tag
+(cd meta-odroid; git checkout 688ff9c5f549f072af204ee64d5ad3ee18b9e47f)
+
+. ./oe-*
+bitbake-layers add-layer ../meta-odroid
+echo "MACHINE = \"odroid-c2\"" >> conf/local.conf 
+
+cd ${CWD}
+
+echo "---------------------------------"
+echo "      Preparation Finished       "
+echo "---------------------------------"
