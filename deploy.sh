a1bd36b4b3d814400c29c64aea212dbd603a998b
H.Heutinck <heutinck.h@gmail.com>
2019-02-13 21:24:10 +0100

    deploy.sh: Comments added

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,21 @@
 # Deploy image to uSD card
 #
 
+# https://github.com/hardkernel/u-boot/tree/odroidxu3-v2012.07/sd_fuse/hardkernel_1mb_uboot
+
+# ----------------------------------------------------
+# Offset Start-End  File/Contents                    Size (byes)
+# 0x000000 - 0x00200   MBR                             0x00200
+# 0x000200 - 0x03F00!! bl1.bin.hardkernel              0x03D00
+# 0x003E00 - 0x07700   bl2.bin.hardkernel.1mb_uboot    0x03900
+#        
+# 0x007E00 - 0x09dA20  u-boot-dtb.bin                  0x95C20
+# 0x107E00 - 0x147E00  tzsw.bin.hardkernel             0x40000
+#
+
+
+
+
 # Parameters
 DEVICE="$1"
 CWD=`pwd`
