854771d04989d9dcbb9c366e98617e03c80b1bd1
H.Heutinck <heutinck.h@gmail.com>
2019-02-14 20:47:36 +0100

    WA: Kernel configuration support

--- prepare.sh
+++ prepare.sh
@@ -8,6 +8,7 @@ set -e
 CWD=`pwd`
 YOCTO_FLAVOUR="thud"
 YOCTO_DIR="yocto-odroid"
+PATCH_FILE=`pwd`/resources/0001-odroid-xu4-hc2-linux-support-workaround.patch
 
 # Checks
 if [ -d ${YOCTO_DIR} ]; then
@@ -20,6 +21,9 @@ git clone -b ${YOCTO_FLAVOUR} git://git.yoctoproject.org/poky.git ${YOCTO_DIR}
 cd ${YOCTO_DIR}
 git clone -b ${YOCTO_FLAVOUR} git://github.com/akuster/meta-odroid
 cp -r layers/meta-hh ${YOCTO_DIR}/.
+
+# Apply work around patches
+(cd meta-odroid; patch -p0 < ${PATCH_FILE})
 
 # Move to version supporting odroid c2 (workaround for nice branch/tag
 (cd meta-odroid; git checkout 688ff9c5f549f072af204ee64d5ad3ee18b9e47f)
