25ea3fc22307006ccea7da76d75ab93d9d1b2e87
H.Heutinck <heutinck.h@gmail.com>
2019-02-21 21:22:15 +0100

    Added bootloaders into wic image manually

--- deploy.sh
+++ deploy.sh
@@ -20,30 +20,43 @@
 
 
 # Parameters
-DEVICE="$1"
+DEVICE="$1" # /dev/<device> or IMG (to create a wic image ready for dd to uSD)
 CWD=`pwd`
 YOCTO_DIR="yocto-odroid"
 IMG_DIR="${YOCTO_DIR}/build/tmp/deploy/images/*/"
 RESOURCE_DIR="resources"
+DEPLOY_DIR="deploy"
 
-# Checks
-echo "_${DEVICE}" | grep "/dev/mmcblk" 2>&1 >/dev/null
-if [ $? -ne 0 ]; then
-    echo "ERROR: Invalid target device specified [${DEVICE}], please verify...."
-    exit 1
+if [ "_${DEVICE}" = "_IMG" ]; then
+
+    IMG_NAME="hh-image-odroid-xu4-hc2.wic"
+    echo "Preparing wic image"
+    mkdir -p ${DEPLOY_DIR}
+    cp yocto-odroid/build/tmp/deploy/images/odroid-xu4-hc2/core-image-minimal-odroid-xu4-hc2.wic ${DEPLOY_DIR}/${IMG_NAME}
+
+    # Add bootloaders
+
+    INPUT_FILE=`find ${IMG_DIR} -name u-boot-dtb.bin`
+    dd if=${RESOURCE_DIR}/bl1.bin.hardkernel           of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=1
+    dd if=${RESOURCE_DIR}/bl2.bin.hardkernel.1mb_uboot of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=31
+    dd if=${INPUT_FILE}                                of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=63
+    dd if=${RESOURCE_DIR}/tzsw.bin.hardkernel          of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=2111
+
+    sudo sync
+
+    # Execution
+    #INPUT_FILE=`find ${IMG_DIR} -name *.wic | grep -v rootfs`
+    #dd if=${INPUT_FILE} of=${DEVICE} bs=1M iflag=fullblock oflag=direct conv=fsync status=progress
+
+else
+
+    # Checks
+    echo "_${DEVICE}" | grep "/dev/mmcblk" 2>&1 >/dev/null
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Invalid target device specified [${DEVICE}], please verify...."
+        exit 1
+    fi
 fi
 
-# Execution
-#INPUT_FILE=`find ${IMG_DIR} -name *.wic | grep -v rootfs`
-#dd if=${INPUT_FILE} of=${DEVICE} bs=1M iflag=fullblock oflag=direct conv=fsync status=progress
-
-# Add bootloaders
-INPUT_FILE=`find ${IMG_DIR} -name u-boot-dtb.bin`
-dd if=${RESOURCE_DIR}/bl1.bin.hardkernel           of=${DEVICE} iflag=dsync oflag=dsync seek=1
-dd if=${RESOURCE_DIR}/bl2.bin.hardkernel.1mb_uboot of=${DEVICE} iflag=dsync oflag=dsync seek=31
-dd if=${INPUT_FILE}                                of=${DEVICE} iflag=dsync oflag=dsync seek=63
-dd if=${RESOURCE_DIR}/tzsw.bin.hardkernel          of=${DEVICE} iflag=dsync oflag=dsync seek=2111
-
-sudo sync
 cd ${CWD}
 
