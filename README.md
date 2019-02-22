8deb6914abca6f3edf0f353e2022cd2d279df4d8
H.Heutinck <heutinck.h@gmail.com>
2019-02-22 21:30:29 +0100

    Dropped WIC idea

--- README.md
+++ README.md
@@ -6,11 +6,35 @@ i.c.w. odroid support layers
 Goal:
 
 - I - Release Reproducability:
-    - a: Fresh Yocto based build (thud at the moment)
-          - Prepare and build scripts: DONE
+DONE - a: Fresh Yocto based build (thud at the moment)
+DONE       - Prepare and build scripts
           
     - b: Resulting image directly transferrable / installable on uSD
           - Deploy script: BUSY
+DONE          - Generarte WIC image
+DONE          - Add bootloaders+armtz to image 
+REJECT          - Be able to load linux from wic image
+                  -> u-boot is not able to find Linux
+                      but it does see partitions
+                     > mmc part
+                       Partition Map for MMC device 0  --   Partition Type: DOS
+
+                       Part    Start Sector    Num Sectors     UUID            Type
+                         1     8192            42598           98d225a3-01     0c Boot
+                         2     57344           74268           98d225a3-02     83
+
+                     > mmc read 0x43000000 0x74268 0x1c0
+                     > md 0x43000000 50
+                      43000000: 6f646461 355f736e 2e32312e 726f2e34    addons_5.12.4.or
+                      43000010: 742e6769 782e7261 62200a7a 35303737    ig.tar.xz. b7705
+                      43000020: 32323231 33363662 63353963 65336162    1222b663c95cba3e
+                      43000030: 31653532 36326338 20643738 34323431    25e18c2687d 1424
+                      43000040: 646b2038 616c7065 2d616d73 6f646461    8 kdeplasma-addo
+                      43000050: 355f736e 2e32312e 75302d34 746e7562    ns_5.12.4-0ubunt
+               -> WIC Images are very limited in their options (not supporting adding raw files
+                     like the bl1.bin.hardkernel etc, so manual HDD img is the next step
+BUSY:          - Generate a manual sdimg
+
 
     - c: Verified on Odroid XU4-HC2
 
--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,24 @@
 
 # https://github.com/hardkernel/u-boot/tree/odroidxu3-v2012.07/sd_fuse/hardkernel_1mb_uboot
 
-# ----------------------------------------------------
-# Offset Start-End  File/Contents                    Size (byes)
-# 0x000000 - 0x00200   MBR                             0x00200
-# 0x000200 - 0x03F00!! bl1.bin.hardkernel              0x03D00
-# 0x003E00 - 0x07700   bl2.bin.hardkernel.1mb_uboot    0x03900
-#        
-# 0x007E00 - 0x09dA20  u-boot-dtb.bin                  0x95C20
-# 0x107E00 - 0x147E00  tzsw.bin.hardkernel             0x40000
-#
+# From hardkernel info
 
+# USD Image (eMMC image block -1)
+# -------------------------------------------------------------------------------------------
+#                    Offset                        |                     |       Size       |
+# Block   Start-End (dec)       Start-End (hex)    | File/Contents       |  (dec)     (hex) | 
+# -------------------------------------------------------------------------------------------
+# 0       00000000 - 00000512   0x000000 - 0x000200  <empty>                  512    0x00200
+# 1       00000512 - 00016128   0x000200 - 0x003F00  bl1.bin.hardkernel     15616    0x03D00
+# !!!OVERLAP!!!
+# 31      00015872 - 00030464   0x003E00 - 0x007700  bl2.bin.hardkernel     14592    0x03900
+#
+# 63      00032256 - 00630525   0x007E00 - 0x099EFD  u-boot-dtb.bin        598269    0x920FD
+#
+# 2111    01080832 - 01342976   0x107E00 - 0x147E00  tzsw.bin.hardkernel   262144    0x40000
+# -------------------------------------------------------------------------------------------
+# Partition 1: Rootfs
+# 3000
 
 
 
@@ -29,24 +37,23 @@ DEPLOY_DIR="deploy"
 
 if [ "_${DEVICE}" = "_IMG" ]; then
 
-    IMG_NAME="hh-image-odroid-xu4-hc2.wic"
-    echo "Preparing wic image"
+    echo "Preparing SD Image"
+    IMG_NAME="hh-image-odroid-xu4-hc2.sdimg"
+    
+    # Create placeholder
     mkdir -p ${DEPLOY_DIR}
-    cp yocto-odroid/build/tmp/deploy/images/odroid-xu4-hc2/core-image-minimal-odroid-xu4-hc2.wic ${DEPLOY_DIR}/${IMG_NAME}
+    dd if=/dev/zero of=${DEPLOY_DIR}/${IMG_NAME} bs=512 count=3000
 
     # Add bootloaders
+    UBOOT_FILE=`find ${IMG_DIR} -name u-boot-dtb.bin`
+    dd if=${RESOURCE_DIR}/bl1.bin.hardkernel            of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=1
+    dd if=${RESOURCE_DIR}/bl2.bin.hardkernel.1mb_uboot  of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=31
+    dd if=${UBOOT_FILE}                                 of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=63
+    dd if=${RESOURCE_DIR}/tzsw.bin.hardkernel           of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=2111
 
-    INPUT_FILE=`find ${IMG_DIR} -name u-boot-dtb.bin`
-    dd if=${RESOURCE_DIR}/bl1.bin.hardkernel           of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=1
-    dd if=${RESOURCE_DIR}/bl2.bin.hardkernel.1mb_uboot of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=31
-    dd if=${INPUT_FILE}                                of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=63
-    dd if=${RESOURCE_DIR}/tzsw.bin.hardkernel          of=${DEPLOY_DIR}/${IMG_NAME} iflag=dsync oflag=dsync seek=2111
-
-    sudo sync
-
-    # Execution
-    #INPUT_FILE=`find ${IMG_DIR} -name *.wic | grep -v rootfs`
-    #dd if=${INPUT_FILE} of=${DEVICE} bs=1M iflag=fullblock oflag=direct conv=fsync status=progress
+    # Add rootfs
+    ROOTFS_FILE=`find ${IMG_DIR} -name *xu4-hc2.ext4`
+    dd if=${ROOTFS_FILE} of=${DEPLOY_DIR}/${IMG_NAME} bs=1M iflag=fullblock oflag=direct conv=fsync status=progress
 
 else
 
